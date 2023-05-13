ac933a6fcbc9ff270aa6cf62135ae519acccbfe8
Ale <alejandra.rodriguezub@uanl.edu.mx>
2023-05-13 03:29:33 -0600

    login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,14 +2,21 @@ import React from "react";
 import LoginForm from "../../Components/LoginForm/LoginForm";
 import './Login.css';
 import { getUserByEmail } from "../../services/usersService";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+
+    const navigate = useNavigate();
+
     return(
         <div className="Login-page">
             <h1>Aquí va la imagen del logo</h1>
             <LoginForm onLogin={async (email,password) => {
                 var userdata = await getUserByEmail(email);
-                console.log(userdata.email + "kjdaf " + userdata.password);
+
+                if (email == userdata.email && password == userdata.password){
+                    navigate("/main");
+                }
             }}/>
         </div>
     );
