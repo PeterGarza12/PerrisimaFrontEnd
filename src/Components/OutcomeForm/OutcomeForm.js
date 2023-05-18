2c6423dc3f7f53ea56fe34943e7f56cbdd14a1a8
PeterGarza12 <pedro.garzagnz@uanl.edu.mx>
2023-05-17 18:31:52 -0600

    ya con outcome

--- src/Components/OutcomeForm/OutcomeForm.js
+++ src/Components/OutcomeForm/OutcomeForm.js
@@ -3,13 +3,20 @@ import { useState } from "react";
 import './OutcomeForm.css'
 
 
-export default function OutcomeForm(){
+export default function OutcomeForm(props){
 
+    const [category, setCategory] = useState(1);
     const [comment, setComment] = useState();
-const [gasto, setGasto] = useState();
+    const [gasto, setGasto] = useState();
+    
+    const handleCategory = (event) =>{
+
+        
+        setCategory(event.target.value);
+        console.log(category);
+    }
 
     const handleComment = (event) =>{
-
         setComment(event.target.value);
     }
 
@@ -18,18 +25,26 @@ const [gasto, setGasto] = useState();
         setGasto(event.target.value);
     }
 
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        let user = JSON.parse(window.sessionStorage.getItem("user"));
+       
+        console.log("userid: ", user);
+        props.onRegisterOutcome(category, comment, gasto, user.id);
+    }
+
 
     return(
         <div className="login-form d-flex justify-content-center">
-            <form className="col-xl-7 col-lg-8 col-md-10 col-11 d-flex flex-column justify-content-between">
+            <form onSubmit={handleFormSubmit} className="col-xl-7 col-lg-8 col-md-10 col-11 d-flex flex-column justify-content-between">
                 <h1 className="h1-form">Gasto</h1>
 
                 <label for="categorias" >Categoría del gasto realizado</label>
-                    <select name="categorias" id="categorias" className="col-11">
-                        <option value="Servicios local">        Servicios local     </option>
-                        <option value="Material pestañas">      Material pestañas   </option>
-                        <option value="Material uñas">          Material uñas       </option>
-                        <option value="Material extensiones">   Material extensiones</option>
+                    <select name="categorias" id="categorias" className="col-11" onClick={handleCategory}>
+                        <option value="1">        Servicios local     </option>
+                        <option value="2">      Material pestañas   </option>
+                        <option value="3">          Material uñas       </option>
+                        <option value="4">   Material extensiones</option>
                     </select>
                     
                 <label>
