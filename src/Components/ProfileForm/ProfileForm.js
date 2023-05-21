a88c4660f92cd0a60881aae36b7adf98d134846f
Ale <alejandra.rodriguezub@uanl.edu.mx>
2023-05-20 19:25:24 -0600

    see password

--- src/Components/ProfileForm/ProfileForm.js
+++ src/Components/ProfileForm/ProfileForm.js
@@ -13,6 +13,12 @@ export default function ProfileForm(){
     const [Username, setUsername] = useState("");
     const [Email, setEmail] = useState("");
     const [Phone, setPhone] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+    const toggleShowPassword = (e) => {
+      e.preventDefault();
+      setShowPassword(!showPassword);
+    };
 
     var user = JSON.parse(window.sessionStorage.getItem("user"));
     // if(user!== null){
@@ -162,49 +168,100 @@ export default function ProfileForm(){
                 </label>
 
                 <div id="passwords" className="col-12 flex-column justify-content-center align-items-center">
-                    <label>
+                    <label className="col-10">
                         Contraseña actual
-                        <input 
-                            id="oldPw" 
-                            className="col-12" 
-                            type="password" 
-                            placeholder="Ingrese su contraseña actual" 
-                            value={ActualPassword} 
-                            onChange={handleActualPasswordChange}
-                            maxLength="8"
-                        />
+                        <div className="d-flex flex-row">
+                            <input 
+                                id="oldPw" 
+                                className="col-12" 
+                                type={showPassword ? 'text' : 'password'} 
+                                placeholder="Ingrese su contraseña actual" 
+                                value={ActualPassword} 
+                                onChange={handleActualPasswordChange}
+                                maxLength="8"
+                            />
+                            <button onClick={toggleShowPassword}>
+                                {
+                                showPassword ? (
+                                    <svg id="eye" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                    </svg>
+                                ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/>
+                                </svg>
+                                )
+                                }
+                            </button>
+                        </div>
                     </label>
 
-                    <label>
+                    <label className="col-10">
                         Contraseña nueva
-                        <input 
-                            id="newPw" 
-                            className="col-12" 
-                            type="password" 
-                            placeholder="Ingrese la contraseña que desea" 
-                            value={NewPassword} 
-                            onChange={handleNewPasswordChange}
-                            maxLength="8"
-                            pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8})/"
-                            onInvalid={e => e.target.setCustomValidity('Contraseña inválida')}
-                            onInput={e => e.target.setCustomValidity('')}
-                        />
+                        <div className="d-flex flex-row">
+                            <input 
+                                id="newPw" 
+                                className="col-12" 
+                                type={showPassword ? 'text' : 'password'} 
+                                placeholder="Ingrese la contraseña que desea" 
+                                value={NewPassword} 
+                                onChange={handleNewPasswordChange}
+                                maxLength="8"
+                                pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8})/"
+                                onInvalid={e => e.target.setCustomValidity('Contraseña inválida')}
+                                onInput={e => e.target.setCustomValidity('')}
+                            />
+                            <button onClick={toggleShowPassword}>
+                                {
+                                showPassword ? (
+                                    <svg id="eye" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                    </svg>
+                                ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/>
+                                </svg>
+                                )
+                                }
+                            </button>
+                        </div>
                     </label>
 
-                    <label>
+                    <label className="col-10"> 
                         Confirmar contraseña nueva
-                        <input 
-                            id="confirmNewPw" 
-                            className="col-12" 
-                            type="password" 
-                            placeholder="Confirme su nueva contraseña" 
-                            value={ConfirmPassword} 
-                            onChange={handleConfirmPasswordChange}
-                            maxLength="8"
-                            pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8})/"
-                            onInvalid={e => e.target.setCustomValidity('Contraseña inválida')}
-                            onInput={e => e.target.setCustomValidity('')}
-                        />
+                        <div className="d-flex flex-row">
+                            <input 
+                                id="confirmNewPw" 
+                                className="col-12" 
+                                type={showPassword ? 'text' : 'password'} 
+                                placeholder="Confirme su nueva contraseña" 
+                                value={ConfirmPassword} 
+                                onChange={handleConfirmPasswordChange}
+                                maxLength="8"
+                                pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8})/"
+                                onInvalid={e => e.target.setCustomValidity('Contraseña inválida')}
+                                onInput={e => e.target.setCustomValidity('')}
+                            />
+                            <button onClick={toggleShowPassword}>
+                                {
+                                showPassword ? (
+                                    <svg id="eye" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                    <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                    </svg>
+                                ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                                    <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z"/>
+                                </svg>
+                                )
+                                }
+                            </button>
+                        </div>
                     </label>
 
                     <button id="savePw"  onClick={changePw} className="btn-form col-xl-6 col-lg-9 col-md-10 col-12" type="submit">
